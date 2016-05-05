bde1a213326aa232abb36a1e768b83ec912207ef
smirnp <smirnp@gmail.com>
2016-05-05 16:58:03 +0300

    Some changes with topology

--- src/storm/benchmark/ThroughputTest.java
+++ src/storm/benchmark/ThroughputTest.java
@@ -30,7 +30,7 @@ import java.util.Random;
 public class ThroughputTest {
     public static String mongoHost = "192.168.13.132";
     public static String dbName = "logging";
-    public static String statsCollection = "storm.processed";
+    public static String statsCollection = "storm.run";
 
     public static class GenSpout extends BaseRichSpout {
         private static final Character[] CHARS = new Character[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
@@ -40,7 +40,7 @@ public class ThroughputTest {
         Random _rand;
         String _id;
         String _val;
-        CommonMongoClient commonMongoClient;
+        //CommonMongoClient commonMongoClient;
         String topoName;
         
         public GenSpout(String topoName, int size) {
@@ -54,16 +54,16 @@ public class ThroughputTest {
             _rand = new Random();
             _id = randString(5);
             _val = randString(_size);
-            commonMongoClient = new CommonMongoClient(ThroughputTest.mongoHost, null, null, ThroughputTest.dbName);
+            //commonMongoClient = new CommonMongoClient(ThroughputTest.mongoHost, null, null, ThroughputTest.dbName);
         }
 
         @Override
         public void nextTuple() {
             tuplesCounter++;
-            TupleStat tupleStat = new TupleStat( topoName, tuplesCounter, new Date(), _size);
-            commonMongoClient.saveObjectToDB(statsCollection, tupleStat);
+            //TupleStat tupleStat = new TupleStat(topoName, tuplesCounter, new Date(), _size);
+            //commonMongoClient.saveObjectToDB(statsCollection+"."+topoName.split("_")[0], tupleStat);
             _collector.emit(new Values(tuplesCounter, _val));
-            itmo.escience.dapris.monitoring.common.Utils.Wait(5000);
+
         }
 
         private String randString(int size) {
@@ -80,7 +80,7 @@ public class ThroughputTest {
         }        
     }
     
-    public static class IdentityBolt extends BaseBasicBolt {
+    public static class IdentityBolt extends BaseBasicBolt{
         int taskID;
         String hostname;
         String componentID;
@@ -92,7 +92,7 @@ public class ThroughputTest {
         }
         @Override
         public void prepare(Map conf, TopologyContext context){
-            commonMongoClient = new CommonMongoClient(ThroughputTest.mongoHost, null, null, ThroughputTest.dbName);
+            //commonMongoClient = new CommonMongoClient(ThroughputTest.mongoHost, null, null, ThroughputTest.dbName);
             taskID = context.getThisTaskId();
             componentID = context.getComponentId(taskID);
             try {
@@ -109,23 +109,24 @@ public class ThroughputTest {
 
         @Override
         public void execute(Tuple tuple, BasicOutputCollector collector) {
-            DBObject condition = new BasicDBObject();
-            condition.put("tupleID", tuple.getValueByField("id"));
-            condition.put("topoName", topoName);
-            List<TupleStat> tupleStats = commonMongoClient.getObjectsFromDB(ThroughputTest.statsCollection, condition, 1, TupleStat.class);
+//            DBObject condition = new BasicDBObject();
+//            condition.put("tupleID", tuple.getValueByField("id"));
+//            condition.put("topoName", topoName);
+//            List<TupleStat> tupleStats = commonMongoClient.getObjectsFromDB(ThroughputTest.statsCollection+"."+topoName.split("_")[0], condition, 1, TupleStat.class);
 
-            TupleStat tupleStat = tupleStats.get(0);
-            tupleStat.started = new Date();
-            tupleStat.taskID = this.taskID;
-            tupleStat.componentID = this.componentID;
-            tupleStat.hostname = this.hostname;
+//            TupleStat tupleStat = tupleStats.get(0);
+//            tupleStat.started = new Date();
+//            tupleStat.taskID = this.taskID;
+//            tupleStat.componentID = this.componentID;
+//            tupleStat.host = this.hostname;
 
-            commonMongoClient.saveObjectToDB(ThroughputTest.statsCollection, condition, tupleStat);
-
+            //commonMongoClient.saveObjectToDB(ThroughputTest.statsCollection+"."+topoName.split("_")[0], condition, tupleStat);
+            for(int i=0; i<1000; i++)
+                Math.sin(i);
             collector.emit(tuple.getValues());
 
-            tupleStat.finished = new Date();
-            commonMongoClient.saveObjectToDB(ThroughputTest.statsCollection, condition, tupleStat);
+            //tupleStat.finished = new Date();
+            //commonMongoClient.saveObjectToDB(ThroughputTest.statsCollection+"."+topoName.split("_")[0], condition, tupleStat);
         }        
     }
 
@@ -157,11 +158,12 @@ public class ThroughputTest {
     
     //storm jar storm-benchmark-0.0.1-SNAPSHOT-standalone.jar storm.benchmark.ThroughputTest demo 1000000 8 8 8 10000
     public static void main(String[] args) throws Exception {
-        String topoName = args[0]+"_"+new Date().getTime();
-        int size = Integer.parseInt(args[1]);
-        int workers = Integer.parseInt(args[2]);
-        int spout = Integer.parseInt(args[3]);
-        int bolt = Integer.parseInt(args[4]);        
+        String topoName = args[0]; //+"_"+new Date().getTime();
+        int workers = Integer.parseInt(args[1]);
+        int spout = Integer.parseInt(args[2]);
+        int bolt = Integer.parseInt(args[3]);
+        int size = Integer.parseInt(args[4]);
+
         //int maxPending = Integer.parseInt(args[5]);
 
         TopologyBuilder builder = new TopologyBuilder();
@@ -173,12 +175,14 @@ public class ThroughputTest {
         //builder.setBolt("bolt2", new AckBolt(), bolt).shuffleGrouping("spout");
 //        builder.setBolt("count2", new CountBolt(), bolt)
 //                .fieldsGrouping("bolt2", new Fields("id"));
-        
+
         Config conf = new Config();
         conf.setNumWorkers(workers);
         //conf.setMaxSpoutPending(maxPending);
         conf.setNumAckers(0);
-        conf.setStatsSampleRate(0.0001);
+        conf.setNumEventLoggers(0);
+
+        //conf.setStatsSampleRate(0.0001);
         //topology.executor.receive.buffer.size: 8192 #batched
         //topology.executor.send.buffer.size: 8192 #individual messages
         //topology.transfer.buffer.size: 1024 # batched
@@ -188,12 +192,14 @@ public class ThroughputTest {
 //        conf.put("topology.receiver.buffer.size", 8);
 //        conf.put(Config.TOPOLOGY_WORKER_CHILDOPTS, "-Xdebug -Xrunjdwp:transport=dt_socket,address=1%ID%,server=y,suspend=n");
 
-        //StormSubmitter.submitTopology(args[0], conf, builder.createTopology());
+        if (topoName.startsWith("LocalTopo")) {
+            LocalCluster cluster = new LocalCluster();
+            StormTopology topology = builder.createTopology();
+            cluster.submitTopology(topoName, conf, topology);
+            Utils.sleep(5*60*1000);
+        }else{
+            StormSubmitter.submitTopology(topoName, conf, builder.createTopology());
+        }
 
-        LocalCluster cluster = new LocalCluster();
-        StormTopology topology = builder.createTopology();
-        cluster.submitTopology(topoName, conf, topology);
-
-        Utils.sleep(5*60*1000);
     }
 }
